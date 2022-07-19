c75b01f2d0363ea360f3950598ca152665f623db
Emily Santos <eyssantos@gmail.com>
2022-07-19 21:31:21 +0100

    add restaurant and review controllers, views and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+    # member do
+    #   get :restaurant
+    # end
+
+  
 end
